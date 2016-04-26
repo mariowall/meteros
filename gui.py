6f4f685f60718b5634f9f07ef03834406a90c2c7
mariowall <mariowall@outlook.com>
2016-04-26 16:17:21 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,18 +1,16 @@
-from Tkinter import *
-from ScrolledText import *
-import tkFileDialog 
+from tkinter import *
 
 text = Tk()
 text.title("MeterTXT")
 frame = Frame(text).pack(side = TOP)
 def openly():
-  files = tkFileDialog.askopenfile(parent=text,mode='rb',title="Open")
+  files = filedialog.askopenfile(parent=text,mode='rb',title="Open")
   if files != None:
     omg = files.read()
     scrolly.insert("1.0",omg)
     omg.closeg
 def save():
-    file = tkFileDialog.asksaveasfile(mode='w')
+    file = filedialog.asksaveasfile(mode='w')
     if file != None:
         data = scrolly.get('1.0', END+'-1c')
         file.write(data)
@@ -21,7 +19,7 @@ opens = Button(frame ,text='Open',command=openly,bg='yellow')
 opens.pack(side = LEFT)
 save = Button(frame,text='Save',command=save,bg='yellow')
 save.pack(side = LEFT)
-scrolly = ScrolledText(text, width=100, height=50)
+scrolly = Text(text, width=100, height=50)
 scrolly.pack()
 
 
