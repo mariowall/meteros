3401862bbda5cf2f820f4744f3933c8a2f4ea38d
mariowall <mariowall@outlook.com>
2016-04-27 15:21:58 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from tkinter import *
 text = Tk()
 text.title("MeterTXT")
 frame = Frame(text).pack(side = TOP)
+menubar = Menu(text)
 def openly():
   files = filedialog.askopenfile(parent=text,mode='rb',title="Open")
   if files != None:
@@ -14,13 +15,24 @@ def save():
     if file != None:
         data = scrolly.get('1.0', END+'-1c')
         file.write(data)
-        file.close()
-opens = Button(frame ,text='Open',command=openly,bg='yellow')
-opens.pack(side = LEFT)
-save = Button(frame,text='Save',command=save,bg='yellow')
-save.pack(side = LEFT)
+def about():
+ about = Tk()
+ about.title("About")
+ al = Label(text="meterOS (c) copyright meteros inc made by\
+ Prajwal Saokar and Sowmiyan NV.").pack()
+ mainloop()
+
+filemenu = Menu(menubar)
+filemenu.add_command(label="Save", command=save)
+filemenu.add_command(label="Open", command=openly)
+menubar.add_cascade(label="File", menu=filemenu)
+
+about = Menu(menubar)
+about.add_command(label="About", command=about)
+menubar.add_cascade(label='About', menu=about)
+
 scrolly = Text(text, width=100, height=50)
 scrolly.pack()
 
-
+text.config(menu=menubar)
 mainloop()
