af20c2a0b59a0219726a485351f24e9424973d1c
mariowall <mariowall@outlook.com>
2016-04-27 15:34:21 -0400

    Update meterpad.py

--- meterpad.py
+++ meterpad.py
@@ -1,28 +1,38 @@
 from Tkinter import *
-from ScrolledText import *
-import tkFileDialog 
 
 text = Tk()
 text.title("MeterTXT")
 frame = Frame(text).pack(side = TOP)
+menubar = Menu(text)
 def openly():
-  files = tkFileDialog.askopenfile(parent=text,mode='rb',title="Open")
+  files = filedialog.askopenfile(parent=text,mode='rb',title="Open")
   if files != None:
     omg = files.read()
     scrolly.insert("1.0",omg)
-    omg.closeg
+    omg.close()
 def save():
-    file = tkFileDialog.asksaveasfile(mode='w')
+    file = filedialog.asksaveasfile(mode='w')
     if file != None:
         data = scrolly.get('1.0', END+'-1c')
         file.write(data)
-        file.close()
-opens = Button(frame ,text='Open',command=openly,bg='yellow')
-opens.pack(side = LEFT)
-save = Button(frame,text='Save',command=save,bg='yellow')
-save.pack(side = LEFT)
-scrolly = ScrolledText(text, width=100, height=50)
+def about():
+ about = Tk()
+ about.title("About")
+ al = Label(text="meterOS (c) copyright meteros inc made by\
+ Prajwal Saokar and Sowmiyan NV.").pack()
+ mainloop()
+
+filemenu = Menu(menubar)
+filemenu.add_command(label="Save", command=save)
+filemenu.add_command(label="Open", command=openly)
+menubar.add_cascade(label="File", menu=filemenu)
+
+about = Menu(menubar)
+about.add_command(label="About", command=about)
+menubar.add_cascade(label='About', menu=about)
+
+scrolly = Text(text, width=100, height=50)
 scrolly.pack()
 
-
+text.config(menu=menubar)
 mainloop()
