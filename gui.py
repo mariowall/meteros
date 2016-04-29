feb459f85fc34421c0bf346ed10ba48863a7726d
mariowall <mariowall@outlook.com>
2016-04-28 20:16:12 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,38 +1,61 @@
 from tkinter import *
+desktop = Tk()
+desktop.title("Desktop - MeterOS")
 
-text = Tk()
-text.title("MeterTXT")
-frame = Frame(text).pack(side = TOP)
-menubar = Menu(text)
-def openly():
-  files = filedialog.askopenfile(parent=text,mode='rb',title="Open")
-  if files != None:
-    omg = files.read()
-    scrolly.insert("1.0",omg)
-    omg.close()
-def save():
-    file = filedialog.asksaveasfile(mode='w')
-    if file != None:
-        data = scrolly.get('1.0', END+'-1c')
-        file.write(data)
-def about():
- about = Tk()
- about.title("About")
- al = Label(text="meterOS (c) copyright meteros inc made by\
- Prajwal Saokar and Sowmiyan NV.").pack()
- mainloop()
+def searchbar():
+    search = Tk()
+    search.title("Search for Applications")
 
-filemenu = Menu(menubar)
-filemenu.add_command(label="Save", command=save)
-filemenu.add_command(label="Open", command=openly)
-menubar.add_cascade(label="File", menu=filemenu)
+    bar = Entry(search)
+    bar.grid()
+    bar.pack()
+    enter = Button(search,text='Enter')
+    enter.pack()
+def meterpad():
+        
+    text = Tk()
+    text.title("MeterTXT")
+    frame = Frame(text).pack(side = TOP)
+    menubar = Menu(text)
+    def openly():
+      files = filedialog.askopenfile(parent=text,mode='rb',title="Open")
+      if files != None:
+        omg = files.read()
+        scrolly.insert("1.0",omg)
+        omg.close()
+    def save():
+        file = filedialog.asksaveasfile(mode='w')
+        if file != None:
+            data = scrolly.get('1.0', END+'-1c')
+            file.write(data)
+    def about():
+     about = Tk()
+     about.title("About")
+     al = Label(text="meterOS (c) copyright meteros inc made by\
+     Prajwal Saokar and Sowmiyan NV.").pack()
+     mainloop()
 
-about = Menu(menubar)
-about.add_command(label="About", command=about)
-menubar.add_cascade(label='About', menu=about)
+    filemenu = Menu(menubar)
+    filemenu.add_command(label="Save", command=save)
+    filemenu.add_command(label="Open", command=openly)
+    menubar.add_cascade(label="File", menu=filemenu)
 
-scrolly = Text(text, width=100, height=50)
-scrolly.pack()
+    about = Menu(menubar)
+    about.add_command(label="About", command=about)
+    menubar.add_cascade(label='About', menu=about)
 
-text.config(menu=menubar)
+    scrolly = Text(text, width=100, height=50)
+    scrolly.pack()
+
+    text.config(menu=menubar)
+
+image = PhotoImage(file="logo.png")
+
+homeb = Button(desktop,text="",command=searchbar)
+homeb.pack()
+homeb.config(image=image)
+
+meterpad = Button(desktop,text='MeterPAD',command=meterpad)
+meterpad.pack()
+
 mainloop()
